a480e623cc45fa1cbb6e10cf46d456e982a8cd16
Danilo de Leon <cdeleonc4@miumg.edu.gt>
2024-01-19 20:52:16 -0600

    Se agregó firebase al proyecto y ee-validate

--- src/main.js
+++ src/main.js
@@ -3,9 +3,13 @@ import { createPinia } from 'pinia'
 
 //Vuetify
 import 'vuetify/styles'
-import {createVuetify} from 'vuetify'
+import {createVuetify } from 'vuetify'
 import * as components from 'vuetify/components'
 import * as directives from 'vuetify/directives'
+
+//Firebase
+import {VueFire, VueFireAuth} from 'vuefire'
+import {firebaseApp} from './config/firebase'
 
 import App from './App.vue'
 import router from './router'
@@ -16,6 +20,12 @@ const app = createApp(App)
 const vuetify = createVuetify({
     components,
     directives
+})
+
+//Firebase
+app.use(VueFire,{
+    firebaseApp,
+    modules: [VueFireAuth()]
 })
 
 app.use(vuetify)
--- src/views/Login.View.vue
+++ src/views/Login.View.vue
@@ -1,15 +1,33 @@
 <script setup>
+    import {useForm, useField} from 'vee-validate'
+    import {useFirebaseAuth} from 'vuefire'
+    import {loginSchema as validationSchema } from '../validation/loginSchema'
+    import {signInWithEmailAndPassword} from 'firebase/auth'
+    const {handleSubmit} = useForm({validationSchema})
+    const auth = useFirebaseAuth()
+
+    const email = useField('email')
+    const password = useField('password')
+
+
+    const submit = handleSubmit((values)=> {
+       signInWithEmailAndPassword(auth, values.email, values.password)
+        .then((userCredential) => {
+                console.log(userCredential);
+        })
+        .catch(error => {
+            console.log(error.code);
+            console.log(error.message);
+        })
+    })
 
 </script>
 
 <template>
-
-   
    <v-card
     flat
     max-width="600"
     class="mx-auto my-10"
-
    >
         <v-card-title
             class="text-h4 font-weight-bold"
@@ -30,17 +48,25 @@
                 type="email"
                 label="Email"
                 bg-color="blue-grey-lighten-5"
+                class="mb-5"
+                v-model="email.value.value"
+                :error-messages="email.errorMessage.value"
+
             />
 
             <v-text-field
                 type="password"
                 label="Password"
                 bg-color="blue-grey-lighten-5"
+                class="mb-5"
+                v-model="password.value.value"
+                :error-messages="password.errorMessage.value"
             />
 
             <v-btn
                 block
                 color="pink-accent-3"
+                @click="submit"
             >
                 Iniciar Sesión
             </v-btn>
