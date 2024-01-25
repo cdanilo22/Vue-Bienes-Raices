9baa1ee1289febe940c08b47f40599bed0f6986e
Danilo de Leon <cdeleonc4@miumg.edu.gt>
2024-01-24 22:22:49 -0600

    Mapeando state co los camps de nuestro formulario nuevapropiedad

--- src/views/admin/NuevaPropiedadView.vue
+++ src/views/admin/NuevaPropiedadView.vue
@@ -1,6 +1,9 @@
 <script setup>
     import {useForm, useField} from 'vee-validate'
     import {validationSchema, imageSchema} from '../../validation/propiedadSchema'
+    
+    const items = [1,2,3,4,5]
+   
     const {handleSubmit} = useForm({
         validationSchema: {
             ...validationSchema,
@@ -8,7 +11,13 @@
         }
     })
 
-    const items = [1,2,3,4,5]
+   const titulo = useField('titulo')
+   const imagen = useField('imagen')
+   const precio = useField('precio')
+   const habitaciones = useField('habitaciones')
+   const wc = useField('wc')
+   const estacionamiento = useField('estacionamiento')
+   const descripcion = useField('descripcion')
 
     const submit = handleSubmit((values)=>{
         console.log(values);
@@ -35,6 +44,8 @@
             <v-text-field
                 class="mb-5"
                 label="Titulo Propiedad"
+                v-model="titulo.value.value"
+                :error-messages="titulo.errorMessage.value"
             />
 
             <v-file-input
@@ -42,12 +53,16 @@
                 label="Fotografía"
                 prepend-icon="mdi-camera"
                 class="mb-5" 
+                v-model="imagen.value.value"
+                :error-messages="imagen.errorMessage.value"
             />
 
             <v-text-field
                 type="number"
                 class="mb-5"
                 label="Precio"
+                v-model="precio.value.value"
+                :error-messages="precio.errorMessage.value"
             />
             
             <v-row>
@@ -60,6 +75,8 @@
                         label="Habitaciones"
                         class="mb-5"
                         :items="items"
+                        v-model="habitaciones.value.value"
+                        :error-messages="habitaciones.errorMessage.value"
                     />
 
                 </v-col>
@@ -72,6 +89,8 @@
                         label="WC"
                         class="mb-5"
                         :items="items"
+                        v-model="wc.value.value"
+                        :error-messages="wc.errorMessage.value"
                     />
 
                 </v-col>
@@ -84,11 +103,13 @@
                         label="Lugares Estacionamiento"
                         class="mb-5"
                         :items="items"
+                        v-model="estacionamiento.value.value"
+                        :error-messages="estacionamiento.errorMessage.value"
                     />
 
                 </v-col>
             </v-row>
-            <v-textarea class="mb-5" label="Descripción"></v-textarea>
+            <v-textarea class="mb-5" label="Descripción" v-model="descripcion.value.value" :error-messages="descripcion.errorMessage.value"></v-textarea>
 
             <v-checkbox label="piscina"/>
 
