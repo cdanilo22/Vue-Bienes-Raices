c2a5a50be4d57135d13b5cfe06b542b0f54b43e8
Danilo de Leon <cdeleonc4@miumg.edu.gt>
2024-01-30 21:33:40 -0600

    Se realizaron cambios en componente propiedad

--- src/components/Propiedad.vue
+++ src/components/Propiedad.vue
@@ -1,5 +1,12 @@
 <script setup>
-
+    defineProps({
+        propiedad: {
+            type: Object
+        },
+        price:{
+            type: Function
+        }
+    })
 </script>
 
 <template>
@@ -7,7 +14,34 @@
     cols="12"
     md="3"
     >
-        <h2>Propiedad...</h2>
+       <v-card>
+            <v-img :src="propiedad.imagen" height="250" cover/>
+            
+            <v-card-title>
+                {{ propiedad.titulo }}
+            </v-card-title>
+
+            <v-card-text class="text-truncate">
+                {{ propiedad.descripcion }}
+            </v-card-text>
+
+            <v-card-text class="text-h5 font-weight-bold">
+              Precio:  {{ price(propiedad.precio) }}
+            </v-card-text>
+            
+            <template v-slot:actions>
+                <v-btn
+                block
+                color="info"
+                variant="flat"
+                
+                >
+                    Ver Información
+                </v-btn>
+            </template>
+
+
+       </v-card>
     </v-col>
 </template>
 
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,7 +1,7 @@
 <script setup>
  import usePropiedades from '../composables/usePropiedades'
   import Propiedad from '../components/Propiedad.vue'
- const { propiedadesCollection} = usePropiedades()
+ const { propiedadesCollection, priceProperty} = usePropiedades()
 </script>
 
 <template>
@@ -16,6 +16,7 @@
         v-for="propiedad in propiedadesCollection"
         :key="propiedad.id"
         :propiedad="propiedad"
+        :price="priceProperty"
       />
 
     </v-row>
